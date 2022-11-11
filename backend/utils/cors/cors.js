87df722b6a2a0837720e09ee05b1523452b8ce59
Dmitrii Sapegin <lejeboka24@gmail.com>
2022-11-11 14:30:49 +0400

    syntax error fixed

--- backend/utils/cors/cors.js
+++ backend/utils/cors/cors.js
@@ -18,7 +18,7 @@ module.exports.cors = (req, res, next) => {
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-COntrol-Allow-Headers'm requestHeaders);
+    res.header('Access-COntrol-Allow-Headers', requestHeaders);
     return res.end();
   }
   return next();
