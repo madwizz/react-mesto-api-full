c97dfffe91348d46a185a815ecd9eab7b87019a6
Dmitrii Sapegin <lejeboka24@gmail.com>
2022-11-12 16:36:47 +0400

    jwt shenanigans

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const mestodb = require('mongoose');
 const bodyParser = require('body-parser');
@@ -22,6 +24,11 @@ app.use(bodyParser.json());
 mestodb.connect(MONGO_URL);
 app.use(requestLogger);
 app.use(cors);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server is going to crash');
+  }, 0);
+});
 
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateRegister, createUser);
